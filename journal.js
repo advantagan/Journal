09259759bdfda0c841d0dcd16433968f12c26fef
ben <advantagan@gmail.com>
2024-03-31 17:44:18 -0500

    20240331 Added Journals

--- journal.js
+++ journal.js
@@ -1,11 +1,26 @@
+let journalArray = [];
+
 let promptArray = [];
 
 let entryArray = [];
 
 let currentPrompt = "";
 
+let currentjournal = "";
+const newjournalform = document.getElementById("newjournalform");
 const journalpromptform = document.getElementById("journalpromptform");
 const journalentryform = document.getElementById("journalentryform");
+
+function selectJournal(journal) {
+  currentjournal = journal;
+  localStorage.setItem("currentjournal", JSON.stringify(currentjournal));
+  displayJournalButtons();
+  const newjournal_entryprompt = document.getElementById(
+    "newjournal_entryprompt"
+  );
+  newjournal_entryprompt.style.display = "block";
+  renderPromptList();
+}
 
 function selectRandomPrompt() {
   const randomindex = Math.floor(Math.random() * promptArray.length);
@@ -21,16 +36,38 @@ function displayPromptDropDown() {
   });
 }
 
+function showSetupForm() {
+  newjournalform.style.display = "block";
+  journalpromptform.style.display = "none";
+  journalentryform.style.display = "none";
+}
+
 function showPromptForm() {
   journalpromptform.style.display = "block";
+  newjournalform.style.display = "none";
   journalentryform.style.display = "none";
 }
 
 function showEntryForm() {
   journalpromptform.style.display = "none";
+  newjournalform.style.display = "none";
   journalentryform.style.display = "block";
   selectRandomPrompt();
   displayPromptDropDown();
+}
+
+function addNewJournal(e) {
+  e.preventDefault();
+  const journalinput = document.getElementById("journalinput");
+  const text = journalinput.value;
+  if (text.trim().length === 0) {
+    return;
+  }
+  journalArray.push(text);
+  journalinput.value = "";
+  localStorage.setItem("journalArray", JSON.stringify(journalArray));
+  renderJournalList();
+  displayJournalButtons();
 }
 
 function addjournalPrompt(e) {
@@ -40,7 +77,7 @@ function addjournalPrompt(e) {
   if (text.trim().length === 0) {
     return;
   }
-  promptArray.push(text);
+  promptArray.push({ journal: currentjournal, prompt: text });
   journalPromptinput.value = "";
   localStorage.setItem("promptArray", JSON.stringify(promptArray));
   renderPromptList();
@@ -56,11 +93,31 @@ function addJournalEntry(e) {
   saveTaskList();
   renderTaskList();
 }
+function renderJournalList() {
+  const journallist = document.getElementById("journallist");
+  journallist.innerHTML = "";
+  journalArray.forEach((journal) => {
+    journallist.innerHTML += `<li>${journal}</li>`;
+  });
+}
+
 function renderPromptList() {
   const journalPromptlist = document.getElementById("promptlist");
   journalPromptlist.innerHTML = "";
-  promptArray.forEach((prompt) => {
-    journalPromptlist.innerHTML += `<li>${prompt}</li>`;
+  promptArray
+    .filter((item) => item.journal === currentjournal)
+    .forEach((arrayitem) => {
+      journalPromptlist.innerHTML += `<li>${arrayitem.prompt}</li>`;
+    });
+}
+
+function displayJournalButtons() {
+  const journalButtons = document.getElementById("journalbuttons");
+  journalButtons.innerHTML = "";
+  journalArray.forEach((journal) => {
+    journalButtons.innerHTML += `<button class="me-4 btn ${
+      currentjournal == journal ? "btn-info" : "btn-primary"
+    } " onclick="selectJournal('${journal}')">${journal}</button>`;
   });
 }
 
@@ -68,4 +125,10 @@ const displayPromptArray = localStorage.getItem("promptArray");
 if (displayPromptArray !== null) {
   promptArray = JSON.parse(displayPromptArray);
   renderPromptList();
+}
+const displayJournalArray = localStorage.getItem("journalArray");
+if (displayJournalArray !== null) {
+  journalArray = JSON.parse(displayJournalArray);
+  renderJournalList();
+  displayJournalButtons();
 }
